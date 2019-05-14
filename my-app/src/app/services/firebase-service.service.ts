454308c6eda87e913a93be1caf3c12021280440c
maliciacanv <cantenovictoriomaruja@gmail.com>
2019-05-14 17:52:19 -0500

    enviando data de nuevo visitante

--- my-app/src/app/services/firebase-service.service.ts
+++ my-app/src/app/services/firebase-service.service.ts
@@ -6,9 +6,31 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class FirebaseServiceService {
 
+  public Visitor = {
+    nombre: '',
+    correo: '',
+    empresaPorVisitada: '',
+    persona: '',
+    empresaVisitante: '',
+  };
+
   constructor(public firestore: AngularFirestore) { }
 
   getVisitors() {
     return this.firestore.collection('visitors').valueChanges();
   }
+
+  addVisitors(name, email, companyVisitador, comunero, comuneroVisitador){
+  const newObj = {
+    ...this.Visitor,
+    nombre: name,
+    correo: email,
+    empresaPorVisitada: companyVisitador,
+    persona: comunero,
+    empresaVisitante: comuneroVisitador
+  };
+    this.firestore.collection('visitors').add(newObj);
+    console.log('se envio a base de datos', newObj)
+  };
+
 }
