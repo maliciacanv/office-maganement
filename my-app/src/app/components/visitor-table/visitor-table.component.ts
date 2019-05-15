eb8387abb3e1f2d296a35d3db30074cc4a7cfd39
NayRojas <nayllen.rojas@gmail.com>
2019-05-15 01:20:04 -0500

    Obtener fecha de salida (Bug: Iteracion a todos los elementos)

--- my-app/src/app/components/visitor-table/visitor-table.component.ts
+++ my-app/src/app/components/visitor-table/visitor-table.component.ts
@@ -4,26 +4,40 @@ import { AngularFirestoreModule, AngularFirestore } from '@angular/fire/firestor
 import { FirebaseServiceService } from '../../services/firebase-service.service';
 
 
-
 @Component({
   selector: 'app-visitor-table',
   templateUrl: './visitor-table.component.html',
   styleUrls: ['./visitor-table.component.css']
 })
 export class VisitorTableComponent implements OnInit {
+  today: any = '';
   visitors: any;
  filterVisitante = '';
 
-  constructor(public firestore: FirebaseServiceService) { 
+  constructor(public firestore: FirebaseServiceService) {
+
     this.firestore.getVisitors().subscribe((data) => {
-      console.log(data)
       this.visitors = data;
-    })
+      
+    });
 
   }
 
   ngOnInit() {
   }
 
+  getOutVisitor(obj: any, id: any) {
+const arraysito = [];
+arraysito.push(obj)
+const obje = arraysito.map((ele:any) => {
+      console.log(ele)
+      if(ele.id === id){
+        return this.today= new Date();
+      }
+    })
+this.today = obje;
 
+  // this.firestore.getDateForVisitorOut(obj, id);
+  console.log(obj, id);
+  }
 }
--- my-app/src/app/services/firebase-service.service.ts
+++ my-app/src/app/services/firebase-service.service.ts
@@ -12,12 +12,14 @@ export class FirebaseServiceService {
 
   constructor(public firestore: AngularFirestore,
               public auth: AngularFireAuth) { }
+  public info = [];
   public Visitor = {
     nombre: '',
     correo: '',
     empresaPorVisitada: '',
     persona: '',
     empresaVisitante: '',
+    fechaDeSalida:''
   };
 
   getVisitors() {
@@ -34,8 +36,39 @@ export class FirebaseServiceService {
     correo: email,
     empresaPorVisitada: companyVisitador,
     persona: comunero,
-    empresaVisitante: comuneroVisitador
+    empresaVisitante: comuneroVisitador,
   };
   this.firestore.collection('visitors').add(newObj);
+  this.info.push(newObj);
+  
+  }
+
+// getCollection(){
+//   this.firestore.collection('visitors').get().then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//         // doc.data() is never undefined for query doc snapshots
+//         console.log(doc.id, ' => ', doc.data());
+//     });
+// });
+// }
+
+  getDateForVisitorOut(obj: any, id: any) {
+  const gettingData = this.getVisitors();
+  this.info.push(gettingData);
+
+  this.info = this.info.map(ele => {
+    let objModif;
+    if (ele.id === id) {
+      objModif = {
+        ...obj,
+        fechaDeSalida: new Date(),
+    };
+    console.log(objModif.fechaDeSalida);
+
+      return objModif;
+  }
+    return ele;
+  });
+
   }
 }
